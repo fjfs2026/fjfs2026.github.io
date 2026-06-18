b1ccc17f9166d302eb0d86a5c8f9858c0b45d971
fjfs2026 <desha.r2049@gmail.com>
2026-06-18 02:06:53 +0200

    Add files via upload

--- catalog-config.js
+++ catalog-config.js
@@ -1,3 +1,2 @@
-// Сюда вставляется CSV-ссылка опубликованной Google Таблицы.
-// Пример: https://docs.google.com/spreadsheets/d/ID/gviz/tq?tqx=out:csv&sheet=Каталог
+// Публичная CSV-ссылка Google Таблицы с меню.
 window.CATALOG_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQvME1oYerxh0AxmPC03dYrQOXAXljckYbyT4eHninXeWrubMAYNetpenKLuUYcszXLc_jeQFuI8HuT/pub?gid=0&single=true&output=csv";
